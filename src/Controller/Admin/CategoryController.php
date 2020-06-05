527aad2e61d972cd87e0313443a4a2d67f56b778
Davy <letellier.davy@gmail.com>
2020-06-05 16:58:19 +0200

    Liste et création de catégories

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/categorie")
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(CategoryRepository $repository)
+    {
+        $categories = $repository->findAll();
+
+        return $this->render(
+            'admin/category/index.html.twig',
+            [
+                'categories' => $categories
+            ]
+        );
+    }
+
+    /**
+     * @Route("/edition")
+     */
+    public function edit(Request $request, EntityManagerInterface $manager)
+    {
+        $category = new Category();
+
+        //création du formulaire relié à la catégorie
+        $form = $this->createForm(CategoryType::class, $category);
+
+        // le formulaire analyse la requête
+        // et sette les valeurs des attributs de l'objet Category
+        // avec les valeurs saisies dans le formulaire
+        $form->handleRequest($request);
+
+        dump($category);
+
+        // si le formulaire a été envoyé
+        if ($form->isSubmitted()) {
+            // si les validations à partir des annotations
+            // dans l'entité Catégory sont ok
+            if ($form->isValid()) {
+                // enregistrement en bdd par le gestionnaire d'entité
+                $manager->persist($category);
+                $manager->flush();
+
+                $this->addFlash('success', 'La catégorie est enregistrée');
+
+                return $this->redirectToRoute('app_admin_category_index');
+            }
+        }
+
+        return $this->render(
+            'admin/category/edit.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+}
+
